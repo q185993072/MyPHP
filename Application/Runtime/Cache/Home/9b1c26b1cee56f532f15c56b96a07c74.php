bb51cd30424c982b58d5be3b180a83ea9ad9cddb
dong <185993072@qq.com>
2016-03-31 16:22:44 +0800

    2016.3.31 add dong

--- Application/Runtime/Cache/Home/9b1c26b1cee56f532f15c56b96a07c74.php
+++ Application/Runtime/Cache/Home/9b1c26b1cee56f532f15c56b96a07c74.php
@@ -5,6 +5,8 @@
     <title>首页</title>
     <link rel="stylesheet" type="text/css" href="/Public/css/home_base.css"/>
     <link rel="stylesheet" type="text/css" href="/Public/css/home_index.css"/>
+    <script type="text/javascript" src="/Public/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/Public/js/home_base.js"></script>
 </head>
 <body>
 <div id="top">
@@ -12,8 +14,8 @@
         <span><a href="">设为首页</a></span>
         <span><a href="">收藏本站</a></span>
         <div class="login">
-            <span><a href="">登录</a></span>
-            <span><a href="">立即注册</a></span>
+            <span class="login_1"><a href="">登录</a></span>
+            <span class="register"><a href="">立即注册</a></span>
         </div>
     </div>
 </div>
@@ -32,7 +34,7 @@
 </div>
 <div id="nav">
     <ul>
-        <li style="background-color:#FE9900"><a href="">首页</a></li>
+        <li class="yellow"><a href="">首页</a></li>
         <li><a href="">论坛</a></li>
         <li><a href="">分类信息</a></li>
         <li><a href="">新闻资讯</a></li>
@@ -171,11 +173,22 @@
             </div>
             <div class="right_login">
                 HI!您好<br/>
-                <button type="button">会员登录</button>
-                <button type="button">马上注册</button>
+                <button type="button" class="login">会员登录</button>
+                <button type="button" class="register">马上注册</button>
             </div>
             <div class="photo">
                 <img src="/Public/images/photo.png" />
+            </div>
+        </div>
+        <!--登录页面-->
+        <div class="login_list">
+            <p>快速登录</p>
+            <div class="login_list_1">
+                <form method="post" action="/home/index/login_save">
+                    <p><label>用户名: </label><input type="text" name="username" placeholder="请填写用户名"/></p>
+                    <p><label>密码: </label><input type="password" name="password" placeholder="请正确填写密码"/></p>
+                    <input type="submit" value="登录" class="submit"/>
+                </form>
             </div>
         </div>
 
@@ -204,11 +217,64 @@
     <!--右边结束-->
 </div>
 <!--主要内容结束-->
+
     <!--广告开始-->
  <div class="advert3">
 
  </div>
     <!--广告结束-->
 
+
+<div id="footer">
+    <div class="user">
+        <p>关于我们</p>
+        <ul>
+            <li>关于我们</li>
+            <li>站点地图</li>
+            <li>版权说明</li>
+        </ul>
+    </div>
+    <div class="lianxi">
+        <p>联系万家</p>
+        <ul>
+            <li>联系我们</li>
+            <li>意见箱</li>
+        </ul>
+    </div>
+    <div class="hezuo">
+        <p>合作交流</p>
+        <ul>
+            <li>加入我们</li>
+            <li>广告服务</li>
+        </ul>
+    </div>
+
+    <p></p>
+
+    <div class="xiazai">
+        <p>下载客户端</p>
+        <ul>
+            <li><a href=""><i class="huose"></i><span>掌上生活</span></a></li>
+            <li><a href=""><i class="zixun"></i><span>安徽资讯</span></a></li>
+            <li><a href=""><i class="shequ"></i><span>万家社区</span></a></li>
+        </ul>
+    </div>
+
+    <p style="margin-left:50px;"></p>
+
+    <div class="guanzhu">
+        <p>关注我们</p>
+        <ul>
+            <li><a href=""><i class="chat"></i><span>微信</span></a></li>
+            <li><a href=""><i class="weibo"></i><span>新浪微博</span></a></li>
+        </ul>
+    </div>
+    <div class="banquan">
+        Copyright © 2006-2016 365jia.cn 安徽大尺度网络传媒有限公司版权所有
+    </div>
+    <div class="beian">
+        皖公网安备 34010002000003号 经营许可证号：皖B2-20070027   E-mail: 365jia@365jia.cn
+    </div>
+</div>
 </body>
 </html>
