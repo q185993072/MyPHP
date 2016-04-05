46d4ecc0909fe18c3b7fca3f8eb10f955b894145
LXY <499218100@qq.com>
2016-04-05 12:12:50 +0800

    LXY ADD

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -19,6 +19,10 @@ class IndexController extends Controller
     {
         $this->display();
     }
+    public function gerenzhuye()
+    {
+        $this->display();
+    }
     public function tiezi()
     {
         $this->display();
