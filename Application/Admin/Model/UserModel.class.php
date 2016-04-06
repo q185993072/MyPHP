1f91b108bcb85188394dab930056999a947e568d
dong <185993072@qq.com>
2016-04-06 12:54:44 +0800

    dong add

--- Application/Admin/Model/UserModel.class.php
+++ Application/Admin/Model/UserModel.class.php
@@ -7,9 +7,13 @@ class UserModel extends Model
     const MY_MODEL_TIMES = 4;
     protected $_validate=[
         ['name','','用户名已被注册',Model::EXISTS_VALIDATE ,'unique',UserModel::MY_MODEL_TIMES],
-        ['email','email','请输入正确邮箱'],
+        ['email','email','请填写正确邮箱'],
         ["mobile","11","手机号必须是11位",0,'length'],
         ['mobile','number','请填写数字',Model::EXISTS_VALIDATE]
+    ];
+
+    protected $_auto=[
+        ['password','md5',Model::MODEL_BOTH,'function'],
     ];
 
 }
