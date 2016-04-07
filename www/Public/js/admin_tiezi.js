4265428bf605e2d098983d8b02b774fd8c690b5d
dong <185993072@qq.com>
2016-04-07 13:21:35 +0800

    dong add

--- www/Public/js/admin_tiezi.js
+++ www/Public/js/admin_tiezi.js
@@ -4,8 +4,9 @@
 $(function(){
     var abtn=$('.zlt_border .btn button');
     abtn.click(function(){
+        var value=$('.li_shutiao #tiezi').text();
         $.post('/Admin/index/pinlun',{},function(e){
-            alert(e);
+            alert(value);
         })
     })
 });
