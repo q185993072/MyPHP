30149fcc2022daa9a746d505d39b260b4d80189c
dong <185993072@qq.com>
2016-04-07 11:43:52 +0800

    dong add

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,8 +3,6 @@ return array(
 //'配置项'=>'配置值'
     'DB_TYPE'                => 'mysql', // 数据库类型
     'DB_HOST'                => '192.168.1.104', // 服务器地址
-    //'DB_HOST'                => '192.168.1.104', // 服务器地址
-    'DB_HOST'                => 'localhost', // 服务器地址
     'DB_NAME'                => 'discuz', // 数据库名
     'DB_USER'                => 'root', // 用户名
     'DB_PORT'                => '3306', // 端口
